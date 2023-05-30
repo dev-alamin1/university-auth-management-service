08a2573b9012769aaf5e45eb9e546ef09c1d5085
dev-alamin1 <dev.alaminmondal@gmail.com>
2023-05-30 10:56:43 +0600

    [DONE] create user interface and model

---
+++ src/app/modules/users.model.ts
@@ -0,0 +1,20 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './users/users.interface'
+
+type UserModel = Model<IUser, object> // for static methods
+
+// Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>(
+  {
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true, // for createdat ,updatedat
+  }
+)
+
+//Create a Model.
+const User = model<IUser, UserModel>('User', userSchema)
+export default User
---
+++ src/app/modules/users/users.interface.ts
@@ -0,0 +1,6 @@
+// Create an interface representing a document in MongoDB.
+export type IUser = {
+  id: string
+  role: string
+  password: string
+}
