cc4c60062ee7832c7b1b51a7e7daee7bca3cbbed
dev-alamin1 <dev.alaminmondal@gmail.com>
2023-06-02 06:41:34 +0600

    separate error log and success log

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
-import logger from './shared/logger'
+import { logger, errorLogger } from './shared/logger'
 
 async function bootstrap() {
   try {
@@ -11,7 +11,7 @@ async function bootstrap() {
       logger.info(`University app listening on port ${config.port}`)
     })
   } catch (err) {
-    logger.error('😥 Database connection failed ', err)
+    errorLogger.error('😥 Database connection failed ', err)
   }
 }
 
