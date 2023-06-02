f9594f75f6c78e54355a06cb8637d686ff857914
dev-alamin1 <dev.alaminmondal@gmail.com>
2023-06-02 06:18:46 +0600

    loogere setup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import logger from './shared/logger'
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('😍 Database connected successfully ')
+    logger.info('😍 Database connected successfully ')
     app.listen(config.port, () => {
-      console.log(`University app listening on port ${config.port}`)
+      logger.info(`University app listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('😥 Database connection failed ', err)
+    logger.error('😥 Database connection failed ', err)
   }
 }
 
